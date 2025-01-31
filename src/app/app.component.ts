ebac46185c8415f088da05bec3d74cc061b3d897
sunny <sunnylalwani650@gmail.com>
2025-01-31 13:56:14 +0000

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { StreamAutocompleteTextareaModule, StreamChatModule } from 'stream-chat-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, StreamAutocompleteTextareaModule, StreamChatModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'chat-example';
-}
+export class AppComponent {}
