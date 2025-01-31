1f0d3c7287bcac9625315cc813a1401a5271fb90
sunny <sunnylalwani650@gmail.com>
2025-01-31 14:35:39 +0000

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { User } from 'stream-chat';
+import {
+  ChatClientService,
+  ChannelService,
+  StreamI18nService,
+  StreamAutocompleteTextareaModule,
+  StreamChatModule,
+  ThemeService,
+} from 'stream-chat-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, StreamAutocompleteTextareaModule, StreamChatModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chat-example';
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+    private themeService: ThemeService,
+  ) {
+    // or light
+    this.themeService.theme$.next('dark');
+
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'icy-night-2';
+    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWN5LW5pZ2h0LTIiLCJleHAiOjE3MzgzMjI5MTZ9.GzvCdvKKFxM0XG1-e48LV0KJP8UwkTfhzO1eiFou6Ao';
+    const userName = 'icy';
+
+    const user: User = {
+      id: userId,
+      name: userName,
+      image: `https://getstream.io/random_png/?name=${userName}`,
+    };
+
+    this.chatService.init(apiKey, user, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
 }
- 
